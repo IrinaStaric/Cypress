48ddc7ec2f78388a161a429e1a1dd50a2c72cd7a
Irina Staric <irinastaric@Irinas-MacBook-Pro.local>
2024-12-06 12:37:52 +0000

    Add content to index.js

--- nodeBasics/index.js
+++ nodeBasics/index.js
@@ -2,4 +2,4 @@ const chalk = require('chalk');
 
 console.log(chalk.blue('Hello, this is a blue message!'));
 console.log(chalk.green('This is a green message!'));
-console.log(chalk.red.bold('And this is a bold red message!'));
+console.log(chalk.red.bold('And this is a bold red messages!'));
