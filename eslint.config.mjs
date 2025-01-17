cf9a74691045043f9fc04175ad42f0c3e5f5429c
@irina.staric1 <irina.staric@gmail.com>
2025-01-17 19:35:26 +0000

    Update eslint.config.mjs with recent changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,16 +12,14 @@ export default [
         prettier: require('eslint-plugin-prettier'),
       },
       rules: {
-        'prettier/prettier': 'error', // Enforce Prettier rules as errors
-        'no-console': 'warn', // Warn when console statements are used
-        'no-unused-vars': 'warn', // Warn on unused variables
-        'react/prop-types': 'off', // Disable prop-types rule for React
-        'react/react-in-jsx-scope': 'off', // Disable React-in-JSX-scope rule for React 17+
-        eqeqeq: 'warn', // Warn when `==` or `!=` is used instead of `===` or `!==`
-        'no-trailing-spaces': 'error', // Disallow trailing spaces
-        indent: ['error', 2], // Enforce 2-space indentation
-        semi: ['error', 'always'], // Enforce semicolons
-        quotes: ['error', 'single'], // Enforce single quotes
+        'prettier/prettier': 'error',
+        'no-console': 'warn',
+        'no-unused-vars': 'warn',
+        'eqeqeq': 'warn',
+        'no-trailing-spaces': 'error',
+        'indent': ['error', 2],
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
       },
     },
   ];
